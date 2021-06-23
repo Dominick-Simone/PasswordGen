a4d9a2ca420f1719c78fdc24f6b24fb932cc9f6f
Dominick-Simone <84548650+Dominick-Simone@users.noreply.github.com>
2021-06-22 21:51:16 -0400

    Fixed issues

--- script.js
+++ script.js
@@ -13,60 +13,74 @@ function writePassword(){
 
   if (length < 8 || length > 128) {
       alert("You must enter a length between 8-128")
+      writePassword();
+      return;
+
   } else {
+
       var lowerChoice = confirm("Include lowercase characters in your password?");
+      console.log(lowerChoice)
+
       var upperChoice = confirm("Include uppercase characters in your password?");
+      console.log(upperChoice)
+
       var specialChoice = confirm("Include special characters in your password?");
+      console.log(specialChoice)
+
       var numberChoice = confirm("Include number characters in your password?");
-  }
+      console.log(numberChoice)
+
+  };
 
   if (!lowerChoice && !upperChoice && !specialChoice && !numberChoice) {
 
     alert("You must choose a criteria!");
+    writePassword();
+    return;
 
 } else if (lowerChoice && upperChoice && specialChoice && numberChoice){
 
-    choices = lowerCase.concat(upperCase, special, numbers)
+    choices = lowerCase.concat(upperCase, special, numbers);
 
 } else if (lowerChoice && upperChoice && specialChoice){
 
-    choices = lowerCase.concat(upperCase, special)
+    choices = lowerCase.concat(upperCase, special);
 
 } else if (lowerChoice && upperChoice && numberChoice){
 
-    choices = lowerCase.concat(upperCase, numbers)
+    choices = lowerCase.concat(upperCase, numbers);
 
 } else if (lowerChoice && numberChoice && specialChoice){
 
-    choices = lowerCase.concat(numbers, special)
+    choices = lowerCase.concat(numbers, special);
 
 } else if (upperChoice && numberChoice && specialChoice){
 
-    choices = upperCase.concat(numbers, special)
+    choices = upperCase.concat(numbers, special);
 
 } else if (upperChoice && numberChoice){
 
-    choices = upperCase.concat(numbers)
+    choices = upperCase.concat(numbers);
 
 } else if (upperChoice && specialChoice){
 
-    choices = upperCase.concat(special)
+    choices = upperCase.concat(special);
 
 } else if (upperChoice && lowerChoice){
 
-    choices = upperCase.concat(lowerCase)
+    choices = upperCase.concat(lowerCase);
 
 } else if (lowerChoice && numberChoice){
 
-    choices = lowerCase.concat(numbers)
+    choices = lowerCase.concat(numbers);
 
 } else if (lowerChoice && specialChoice){
 
-    choices = lowerCase.concat(special)
+    choices = lowerCase.concat(special);
 
 } else if (specialChoice && numberChoice){
 
-    choices = special.concat(numbers)
+    choices = special.concat(numbers);
 
 } else if (specialChoice){
 
@@ -84,18 +98,18 @@ function writePassword(){
 
   choices = numbers;
 
-}
+};
 
   for (i=0; i < length; i++){
   character = choices[Math.round(Math.random(0, choices.length) * choices.length)];
   password.push(character);
   };
-  console.log(password)
 
   var passwordText = document.querySelector("#password");
   finalpassword = password.join('');
   passwordText.value = finalpassword;
+  password = [];
 };
-
+  
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
